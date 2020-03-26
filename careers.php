15e424bb6d4dc8a24e80048fb203d02ae9818489
Varun V <varunvirupaksha321@gmail.com>
2020-03-27 00:10:23 +0530

    updated mail php, text align

---
+++ careers.php
@@ -0,0 +1,14 @@
+<?php 
+	$name = $_POST['name'];
+	$grad = $_POST['grad'];
+	$num = $_POST['num'];
+	$email = $_POST['email'];
+	$resume = $_POST['resume'];
+	$cover = $_POST['cover'];
+
+	$to = 'careers@cognitiveworks.ai';
+	$subject = 'Job Application';
+	$message = "Name of Candidate: ".$name."\n" . "Year of Graduation:".$grad."\n"."Email:".$email."\n". "Mobile Number:".$num."\n". "E-mail:".$email."\n"."Resume:".$resume."\n". "Cover Letter:".$cover;
+	
+	mail($to, $subject, $message);
+?>
