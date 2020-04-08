04766a4743ea9cac8cb0a42a670dba176dc12246
Varun V <varunvirupaksha321@gmail.com>
2020-04-08 20:33:05 +0530

    updated careers form

--- careers.php
+++ careers.php
@@ -1,14 +1,20 @@
 <?php 
 	$name = $_POST['name'];
 	$grad = $_POST['grad'];
+	$college = $_POST['college'];
+	$specialization = $_POST['specialization'];
 	$num = $_POST['num'];
+	$num2 = $_POST['alt-num'];
 	$email = $_POST['email'];
+	$email2 = $_POST['alt-email'];
 	$resume = $_POST['resume'];
 	$cover = $_POST['cover'];
 
 	$to = 'careers@cognitiveworks.ai';
+
 	$subject = 'Job Application';
-	$message = "Name of Candidate: ".$name."\n" . "Year of Graduation:".$grad."\n"."Email:".$email."\n". "Mobile Number:".$num."\n". "E-mail:".$email."\n"."Resume Link:".$resume."\n". "Cover Letter:".$cover;
+
+	$message = "Name of Candidate: ".$name."\n" . "College Name: ".$college."\n" . "Year of Graduation: ".$grad."\n" . "Graduation Specialiation: ".$specialization."\n" ."\n"."Email: ".$email."\n". "Mobile Number: ".$num. "Alternate Number: ".$num2."\n". "E-mail: ".$email."\n". "Alternate e-mail: ".$email2."\n"."Resume Link: ".$resume."\n". "Cover Letter Link: ".$cover;
 	
 	mail($to, $subject, $message);
 ?>
